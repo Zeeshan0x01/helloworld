4f82eba2fded5b52a1c79414bb920f5c3b860b99
bushra-qadir <bushraqadir@ciit.net.pk>
2015-11-02 00:03:01 +0500

    My first commit

---
+++ EgitDemo/src/exception/AgeInputVer1.java
@@ -0,0 +1,24 @@
+
+package exception;
+import java.util.*;
+public class AgeInputVer1 
+{
+	private static final String DEFAULT_MESSAGE = "Your age: " ;
+	private Scanner scanner;
+	public AgeInputVer1 ( ) {
+	scanner = new Scanner ( System.in ) ;
+	}
+	public int getAge () {
+	return getAge ( DEFAULT_MESSAGE ) ;
+	}
+	
+	//Using this service class, we can write a program that gets a person’s age and
+	//replies with the year in which the person was born. Notice the program takes into consideration whether the person already had a birthday this year. Here’s the program:
+	public int getAge ( String prompt ) {
+		System.out.print ( prompt ) ;
+		int age = scanner.nextInt () ;
+		return age;
+		}
+
+}
+
