2985f8ce6c2c1aba476e2baab46db3298d51c480
bushra-qadir <bushraqadir@ciit.net.pk>
2015-11-06 13:41:34 +0500

    abc

--- EgitDemo/src/exception/Newclass2.java
+++ EgitDemo/src/exception/Newclass2.java
@@ -1,5 +1,42 @@
 package exception;
 
-public class Newclass2 {
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Newclass2 {
+	
+	private static final String filename = "input.txt";
+	
+	public static void main(String[] args) throws IOException //throws IOException
+	{
+		BufferedReader rd = null;
+		File file=new File(filename);
+		//file.createNewFile();
+		try {
+			// Open the file for reading.
+			rd = new BufferedReader(new FileReader(new File(filename)));
+			
+			// Read all contents of the file.
+			String inputLine = null;
+			while((inputLine = rd.readLine()) != null)
+				System.out.println(inputLine);
+		}
+		catch(IOException ex) {
+			System.err.println("An IOException was caught!");
+			ex.printStackTrace();
+		}
+		finally {
+			// Close the file.
+			try {
+				rd.close();
+			}
+			catch (IOException ex) {
+				System.err.println("An IOException was caught!");
+				ex.printStackTrace();
+			}
+		}
+	}
 }
